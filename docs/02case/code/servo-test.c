b90f92dd0a074f1c235501ce4351c86713fce3b0
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-05-31 11:35:31 -0400

    Added servo code example

---
+++ docs/02case/code/servo-test.c
@@ -0,0 +1,84 @@
+/* 
+ *
+ *  servo tester
+ *  (c) Copyright 2016
+ *  Mark A. Yoder, 20-July-2016
+ *
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "robotics_cape_defs.h"
+
+#define PRU_ADDR		0x4A300000		// Start of PRU memory Page 184 am335x TRM
+#define PRU_LEN			0x80000			// Length of PRU memory
+#define PRU_SHAREDMEM	0x10000			// Offset to shared memory
+
+unsigned int	*prusharedMem_32int_ptr;	// Points to the start of the shared memory
+
+/*******************************************************************************
+* int send_servo_pulse_us(int ch, int us)
+* 
+* Sends a single pulse of duration us (microseconds) to a single channel (ch)
+*******************************************************************************/
+int send_servo_pulse_us(int ch, int us) {
+	// Sanity Checks
+	if(ch<1 || ch>SERVO_CHANNELS){
+		printf("ERROR: Servo Channel must be between 1&%d\n", SERVO_CHANNELS);
+		return -1;
+	} if(prusharedMem_32int_ptr == NULL){
+		printf("ERROR: PRU servo Controller not initialized\n");
+		return -1;
+	}
+	// PRU runs at 200Mhz. find #loops needed
+	unsigned int num_loops = ((us*200.0)/PRU_SERVO_LOOP_INSTRUCTIONS); 
+	// printf("num_loops: %d\n", num_loops);
+	// write to PRU shared memory
+	prusharedMem_32int_ptr[ch-1] = num_loops;
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned int	*pru;		// Points to start of PRU memory.
+	int	fd;
+	printf("Servo tester\n");
+	
+	fd = open ("/dev/mem", O_RDWR | O_SYNC);
+	if (fd == -1) {
+		printf ("ERROR: could not open /dev/mem.\n\n");
+		return 1;
+	}
+	pru = mmap (0, PRU_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, fd, PRU_ADDR);
+	if (pru == MAP_FAILED) {
+		printf ("ERROR: could not map memory.\n\n");
+		return 1;
+	}
+	close(fd);
+	printf ("Using /dev/mem.\n");
+	
+	prusharedMem_32int_ptr = pru + PRU_SHAREDMEM/4;	// Points to start of shared memory
+
+	// while(1) {
+	// 	printf("value to store: ");
+	// 	scanf("%d", &value);
+	// 	printf("Storing: %d in %lx\n", value, addr);
+	// 	pru[addr/4] = value;
+	// }
+	
+	int i;
+	while(1) {
+		for(i=1; i<=SERVO_CHANNELS; i++) {
+			send_servo_pulse_us(i, 10*i);
+		}
+		usleep(200);
+	}
+	
+	if(munmap(pru, PRU_LEN)) {
+		printf("munmap failed\n");
+	} else {
+		printf("munmap succeeded\n");
+	}
+}
+
