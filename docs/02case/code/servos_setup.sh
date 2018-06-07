61ceb37069ae5ad6102de98c126fa1c96ed4b424
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-06-07 13:19:05 -0400

    Added Blue

--- docs/02case/code/servos_setup.sh
+++ docs/02case/code/servos_setup.sh
@@ -5,11 +5,15 @@ echo -n $machine
 if [ $machine = "Black" ]; then
     echo " Found"
     pins="P8_27 P8_28 P8_29 P8_30 P8_39 P8_40 P8_41 P8_42"
+elif [ $machine = "Blue" ]; then
+    echo " Found"
+    pins=""
 elif [ $machine = "PocketBeagle" ]; then
     echo " Found"
     pins="P2_35 P1_35 P1_02 P1_04"
 else
     echo " Not Found"
+    pins=""
 fi
 
 for pin in $pins
