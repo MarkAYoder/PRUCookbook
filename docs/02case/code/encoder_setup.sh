68cb5eee72270de647cd83021aa1dcfef80f7713
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-06-08 12:45:00 -0400

    Added pruin configuration

--- docs/02case/code/encoder_setup.sh
+++ docs/02case/code/encoder_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Configure the PRU pins based on which Beagle is running
+# Configure the pins based on which Beagle is running
 machine=$(awk '{print $NF}' /proc/device-tree/model)
 echo -n $machine
+
+# Configure eQEP pins
 if [ $machine = "Black" ]; then
     echo " Found"
     pins="P9_92 P9_27 P8_35 P8_33 P8_12 P8_11 P8_41 P8_42"
@@ -20,5 +22,28 @@ for pin in $pins
 do
     echo $pin
     config-pin $pin qep
+    config-pin -q $pin
+done
+
+##########################################
+# Configure PRU pins
+if [ $machine = "Black" ]; then
+    echo " Found"
+    pins="P8_16 P8_15"
+elif [ $machine = "Blue" ]; then
+    echo " Found"
+    pins=""
+elif [ $machine = "PocketBeagle" ]; then
+    echo " Found"
+    pins="P2_09 P2_18"
+else
+    echo " Not Found"
+    pins=""
+fi
+
+for pin in $pins
+do
+    echo $pin
+    config-pin $pin pruin
     config-pin -q $pin
 done
