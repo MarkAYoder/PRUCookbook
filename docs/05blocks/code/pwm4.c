50201f9c83895f652e206101a4f54c09ce23317a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-06-11 11:49:22 -0400

    Adding shared memory

--- docs/05blocks/code/pwm4.c
+++ docs/05blocks/code/pwm4.c
@@ -1,58 +1,48 @@
-// This code does three parallel PWM channels.
+// This code does MAXCH parallel PWM channels.
+// It's period is 3 us
 #include <stdint.h>
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
+
+#define PRU0_DRAM		0x00000			// Offset to DRAM
+unsigned int *pru0_dram = PRU0_DRAM;
+
+#define MAXCH	4	// Maximum number of channels
 
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
 void main(void)
 {
-	uint32_t gpio;
 	uint32_t ch;
-	uint32_t on[] = {1, 2, 3};
-	uint32_t off[] = {3, 2, 1};
-	uint32_t onCount[3], offCount[3];
+	uint32_t on[]  = {1, 2, 3, 4};	// Number of cycles to stay on
+	uint32_t off[] = {4, 3, 2, 1};	// Number to stay off
+	uint32_t onCount[MAXCH];		// Current count
+	uint32_t offCount[MAXCH];
 
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	onCount[ch] = on[ch];
-	offCount[ch]= off[ch];
+	// Initialize the channel counters.
+	for(ch=0; ch<MAXCH; ch++) {
+		pru0_dram[ch] = on[ch];		// Copy to DRAM0 so the ARM can change it
+		pru0_dram[ch+MAXCH] = off[ch];	// Copy oafter the on array
+		onCount[ch] = on[ch];
+		offCount[ch]= off[ch];
+	}
 
 	while (1) {
-		ch=0;
-		if(onCount[ch]) {
-			onCount[ch]--;
-			__R30 |= 0x1<<ch;		// Set the GPIO pin to 1
-		} else if(offCount[ch]) {
-			offCount[ch]--;
-			__R30 &= ~(0x1<<ch);		// Clear the GPIO pin
-		} else {
-			onCount[ch] = on[ch];
-			offCount[ch]= off[ch];
-		}
-		ch=1;
-		if(onCount[ch]) {
-			onCount[ch]--;
-			__R30 |= 0x1<<ch;		// Set the GPIO pin to 1
-		} else if(offCount[ch]) {
-			offCount[ch]--;
-			__R30 &= ~(0x1<<ch);		// Clear the GPIO pin
-		} else {
-			onCount[ch] = on[ch];
-			offCount[ch]= off[ch];
-		}
-		ch=2;
-		if(onCount[ch]) {
-			onCount[ch]--;
-			__R30 |= 0x1<<ch;		// Set the GPIO pin to 1
-		} else if(offCount[ch]) {
-			offCount[ch]--;
-			__R30 &= ~(0x1<<ch);		// Clear the GPIO pin
-		} else {
-			onCount[ch] = on[ch];
-			offCount[ch]= off[ch];
+		for(ch=0; ch<MAXCH; ch++) {
+			if(onCount[ch]) {
+				onCount[ch]--;
+				__R30 |= 0x1<<ch;		// Set the GPIO pin to 1
+			} else if(offCount[ch]) {
+				offCount[ch]--;
+				__R30 &= ~(0x1<<ch);		// Clear the GPIO pin
+			} else {
+				onCount[ch] = pru0_dram[ch];		// Read from DRAM0
+				offCount[ch]= pru0_dram[ch+MAXCH];
+			}
 		}
 	}
 }
