da001849f74c36e310c4249029d7b8fb6b2366b1
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-06-11 12:57:26 -0400

    Added DRAM code

--- docs/05blocks/code/pwm5.c
+++ docs/05blocks/code/pwm5.c
@@ -4,6 +4,11 @@
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
 
+#define PRU0_DRAM		0x00000			// Offset to DRAM
+// Skip the first 0x200 byte of DRAM since the Makefile allocates
+// 0x100 for the STACK and 0x100 for the HEAP.
+unsigned int *pru0_dram = (unsigned int *) (PRU0_DRAM + 0x200);
+
 #define MAXCH	4	// Maximum number of channels
 
 #define update(ch) \
@@ -14,8 +19,8 @@
 				offCount[ch]--;			\
 				__R30 &= ~(0x1<<ch);	\
 			} else {					\
-				onCount[ch] = on[ch];	\
-				offCount[ch]= off[ch];	\
+				onCount[ch] = pru0_dram[2*ch];	\
+				offCount[ch]= pru0_dram[2*ch+1];	\
 			}
 
 volatile register uint32_t __R30;
@@ -33,6 +38,8 @@ void main(void)
 
 #pragma UNROLL(MAXCH)
 	for(ch=0; ch<MAXCH; ch++) {
+		pru0_dram[2*ch  ] = on[ch];		// Copy to DRAM0 so the ARM can change it
+		pru0_dram[2*ch+1] = off[ch];	// Interleave the on and off values
 		onCount[ch] = on[ch];
 		offCount[ch]= off[ch];
 	}
