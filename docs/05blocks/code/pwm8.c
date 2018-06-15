3b56b5aa553a114a7775031b1f4a6ea87eb5de6b
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-06-15 15:28:38 -0400

    Added syncing PRUs

--- docs/05blocks/code/pwm8.c
+++ docs/05blocks/code/pwm8.c
@@ -1,8 +1,10 @@
 // This code does MAXCH parallel PWM channels on both PRU 0 and PRU 1
-// All channels start at the same time. But the PRU 1 ch have a difference period
-// It's period is 370ns
+// All channels start at the same time. 
+// It's period is 510ns
 #include <stdint.h>
 #include <pru_cfg.h>
+#include <pru_intc.h>
+#include <pru_ctrl.h>
 #include "resource_table_empty.h"
 
 #define PRU0_DRAM		0x00000			// Offset to DRAM
@@ -27,6 +29,21 @@ volatile unsigned int *pru0_dram = (unsigned int *) (PRU0_DRAM + 0x200);
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
+// Initialize intrupts so the PRUs can be syncronized.
+// PRU1 is started first and then waits for PRU0
+// PRU0 is then started and tells PRU1 when to start going
+#if PRUN==0
+void configIntc(void) {	
+	__R31 = 0x00000000;					// Clear any pending PRU-generated events
+	CT_INTC.CMR4_bit.CH_MAP_16 = 1;		// Map event 16 to channel 1
+	CT_INTC.HMR0_bit.HINT_MAP_1 = 1;	// Map channel 1 to host 1
+	CT_INTC.SICR = 16;					// Ensure event 16 is cleared
+	CT_INTC.EISR = 16;					// Enable event 16
+	CT_INTC.HIEISR |= (1 << 0);			// Enable Host interrupt 1
+	CT_INTC.GER = 1; 					// Globally enable host interrupts
+}
+#endif
+
 void main(void)
 {
 	uint32_t ch;
@@ -34,6 +51,11 @@ void main(void)
 	uint32_t off[] = {4, 3, 2, 1};
 	uint32_t onCount[MAXCH], offCount[MAXCH];
 	register uint32_t Rtmp;
+
+#if PRUN==0
+	CT_CFG.GPCFG0 = 0x0000;				// Configure GPI and GPO as Mode 0 (Direct Connect)
+	configIntc();						// Configure INTC
+#endif
 
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
@@ -48,8 +70,18 @@ void main(void)
 	Rtmp = __R30;
 
 	while (1) {
+#if PRUN==0
+#define PRU0_PRU1_EVT 16
+	__R31 = (PRU0_PRU1_EVT-16) | (0x1<<5);	//Tell PRU 1 to start
+	__delay_cycles(8);
+#endif
+#if PRUN==1
+	while((__R31 & (0x1<<31))==0) {			// Wait for PRU 0
+	}
+	CT_INTC.SICR = 16;					// Ensure event 16 is cleared
+#endif
+		__R30 = Rtmp;
 		update(0)
 		update(1)
-		__R30 = Rtmp;
 	}
 }
