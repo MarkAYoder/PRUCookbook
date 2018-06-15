62b60011f302aa9952fda4326bc3fb43edcfd35c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-06-15 10:05:01 -0400

    Added synced channels

--- docs/05blocks/code/pwm6.c
+++ docs/05blocks/code/pwm6.c
@@ -1,5 +1,5 @@
 // This code does MAXCH parallel PWM channels.
-// It's period is 510ns.
+// All channels start at the same time. It's period is 510ns
 #include <stdint.h>
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
@@ -14,10 +14,10 @@ unsigned int *pru0_dram = (unsigned int *) (PRU0_DRAM + 0x200);
 #define update(ch) \
 			if(onCount[ch]) {			\
 				onCount[ch]--;			\
-				__R30 |= 0x1<<ch;		\
+				Rtmp |= 0x1<<ch;		\
 			} else if(offCount[ch]) {	\
 				offCount[ch]--;			\
-				__R30 &= ~(0x1<<ch);	\
+				Rtmp &= ~(0x1<<ch);	\
 			} else {					\
 				onCount[ch] = pru0_dram[2*ch];	\
 				offCount[ch]= pru0_dram[2*ch+1];	\
@@ -32,6 +32,7 @@ void main(void)
 	uint32_t on[]  = {1, 2, 3, 4};
 	uint32_t off[] = {4, 3, 2, 1};
 	uint32_t onCount[MAXCH], offCount[MAXCH];
+	register uint32_t Rtmp;
 
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
@@ -43,11 +44,13 @@ void main(void)
 		onCount[ch] = on[ch];
 		offCount[ch]= off[ch];
 	}
+	Rtmp = __R30;
 
 	while (1) {
 		update(0)
 		update(1)
 		update(2)
 		update(3)
+		__R30 = Rtmp;
 	}
 }
