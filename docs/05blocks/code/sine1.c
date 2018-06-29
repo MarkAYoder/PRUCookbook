e650d081c4d6517642f59c335a2f671ca2f241fd
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-06-29 09:27:46 -0400

    Added #defines for each waveform

--- docs/05blocks/code/sine1.c
+++ docs/05blocks/code/sine1.c
@@ -12,6 +12,8 @@ volatile unsigned int *pru0_dram = (unsigned int *) (PRU0_DRAM + 0x200);
 
 #define MAXT	100	// Maximum number of time samples
 
+#define SINE		// Pick which waveform
+
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
@@ -24,13 +26,26 @@ void main(void)
 	// Generate a periodic sine wave in an array of 100 values - using ints
 	int i;
 	unsigned int waveform[MAXT];
+#ifdef SINE
 	float gain = 50.0f;
 	float phase = 0.0f;
 	float bias = 50.0f;
 	float freq = 2.0f * 3.14159f / 100.0f;
 	for (i=0; i<MAXT; i++){
-	  waveform[i] = (unsigned int)(bias + (gain * sin((i * freq) + phase)));
+		waveform[i] = (unsigned int)(bias + (gain * sin((i * freq) + phase)));
 	}
+#endif
+#ifdef SAWTOOTH
+	for(i=0; i<MAXT; i++) {
+		waveform[i] = i;
+	}
+#endif
+#ifdef TRIANGLE
+	for(i=0; i<MAXT/2; i++) {
+		waveform[i]        = 2*i;
+		waveform[MAXT-i-1] = 2*i;
+	}
+#endif
 
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
