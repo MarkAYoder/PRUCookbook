3604f0ee39e2271257695d6d7489ac0f86e5df4e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-07-02 12:38:27 -0400

    Adding shared mem section

--- docs/05blocks/code/shared.c
+++ docs/05blocks/code/shared.c
@@ -4,24 +4,37 @@
 #include <pru_ctrl.h>
 #include "resource_table_empty.h"
 
-#ifndef PRU_SRAM
-#define PRU_SRAM __far __attribute__((cregister("PRU_SHAREDMEM", near)))
-#define PRU_DRAM __far __attribute__((cregister("PRU_DMEM_1_0",  near)))
-#endif
+#define PRU_SRAM  __far __attribute__((cregister("PRU_SHAREDMEM", near)))
+#define PRU_DMEM0 __far __attribute__((cregister("PRU_DMEM_0_1",  near)))
+#define PRU_DMEM1 __far __attribute__((cregister("PRU_DMEM_1_0",  near)))
 
-/* NOTE:  Allocating shared_freq_x to PRU Shared Memory means that other PRU cores on
+/* NOTE:  Allocating shared_x to PRU Shared Memory means that other PRU cores on
  *        the same subsystem must take care not to allocate data to that memory.
  *		  Users also cannot rely on where in shared memory these variables are placed
  *        so accessing them from another PRU core or from the ARM is an undefined behavior.
  */
-PRU_SRAM volatile uint32_t shared_1;
-PRU_SRAM volatile uint32_t shared_2;
-#pragma DATA_SECTION(shared_3, ".bss")
-volatile uint32_t shared_3;
+volatile uint32_t shared_0;
+PRU_SRAM  volatile uint32_t shared_1;
+PRU_DMEM0 volatile uint32_t shared_2;
+PRU_DMEM1 volatile uint32_t shared_3;
+#pragma DATA_SECTION(shared_4, ".bss")
+volatile uint32_t shared_4;
+
+/* NOTE:  Here we pick where in memory to store shared_5.  The stack and
+ *		  heap take up the frist 0x200 words, so we must start after that.
+ *		  Since we are hardcoding where things are stored we can share
+ *		  this between the PRUs and the ARM.
+*/
+#define PRU0_DRAM		0x00000			// Offset to DRAM
+// Skip the first 0x200 bytes of DRAM since the Makefile allocates
+// 0x100 for the STACK and 0x100 for the HEAP.
+volatile unsigned int *shared_5 = (unsigned int *) (PRU0_DRAM + 0x200);
+
 
 int main(void)
 {
-
+	volatile uint32_t shared_6;
+	volatile uint32_t shared_7;
 	/*****************************************************************/
 	/* Access PRU peripherals using Constant Table & PRU header file */
 	/*****************************************************************/
@@ -36,13 +49,14 @@ int main(void)
 	/* C28 defaults to 0x00000000, we need to set bits 23:8 to 0x0100 in order to have it point to 0x00010000	 */
 	PRU0_CTRL.CTPPR0_bit.C28_BLK_POINTER = 0x0100;
 
-	/* Define value of shared_1 */
+	shared_0 =  0xfeef;
 	shared_1 = 0xdeadbeef;
-
-	/* Read PRU Shared RAM 2 memory */
 	shared_2 = shared_2 + 0xfeed;
-	
-	shared_3 = shared_3 + 0xdeed;
+	shared_3 = 0xdeed;
+	shared_4 = 0xbeed;
+	shared_5[0] = 0x1234;
+	shared_6 = 0x4321;
+	shared_7 = 0x9876;
 
 	/* Halt PRU core */
 	__halt();
