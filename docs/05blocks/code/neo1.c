88d12c50caf7f0652949dd024914ba95cbe2982e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-07-02 15:50:58 -0400

    Adding NeoPixels

---
+++ docs/05blocks/code/neo1.c
@@ -0,0 +1,35 @@
+// Control a ws2812 (neo pixel) display
+#include <stdint.h>
+#include <pru_cfg.h>
+#include "resource_table_empty.h"
+
+#define	oneCyclesOn		700/5	// Stay on 700ns
+#define oneCyclesOff	800/5
+#define zeroCyclesOn	350/5
+#define zeroCyclesOff	600/5
+#define out 1		// Bit number to output one
+
+// #define ONE
+
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
+
+void main(void)
+{
+	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
+	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+	while (1) {
+#ifdef ONE
+		__R30 |= 0x1<<out;		// Set the GPIO pin to 1
+		__delay_cycles(oneCyclesOn-1);
+		__R30 &= ~(0x1<<out);	// Clear the GPIO pin
+		__delay_cycles(oneCyclesOff-2);
+#else
+		__R30 |= 0x1<<out;		// Set the GPIO pin to 1
+		__delay_cycles(zeroCyclesOn-1);
+		__R30 &= ~(0x1<<out);	// Clear the GPIO pin
+		__delay_cycles(zeroCyclesOff-2);
+#endif
+	}
+}
---
+++ docs/05blocks/code/neo_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+export PRUN=0
+export TARGET=neo1
+
+# Configure the PRU pins based on which Beagle is running
+machine=$(awk '{print $NF}' /proc/device-tree/model)
+echo -n $machine
+if [ $machine = "Black" ]; then
+    echo " Found"
+    pins="P9_29 "
+elif [ $machine = "Blue" ]; then
+    echo " Found"
+    pins=""
+elif [ $machine = "PocketBeagle" ]; then
+    echo " Found"
+    pins="P1_33"
+else
+    echo " Not Found"
+    pins=""
+fi
+
+for pin in $pins
+do
+    echo $pin
+    config-pin $pin pruout
+    config-pin -q $pin
+done
