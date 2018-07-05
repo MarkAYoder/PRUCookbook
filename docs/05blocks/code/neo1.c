cd7e1fabffcf70dfd54cdac66abc92e93536a1cd
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-07-05 11:34:19 -0400

    Added Discussion

--- docs/05blocks/code/neo1.c
+++ docs/05blocks/code/neo1.c
@@ -3,13 +3,15 @@
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
 
+#define STR_LEN 24
 #define	oneCyclesOn		700/5	// Stay on 700ns
 #define oneCyclesOff	800/5
 #define zeroCyclesOn	350/5
 #define zeroCyclesOff	600/5
+#define resetCycles		60000/5	// Must be at least 50u, use 60u
 #define out 1		// Bit number to output one
 
-// #define ONE
+#define ONE
 
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
@@ -19,7 +21,8 @@ void main(void)
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	while (1) {
+	uint32_t i;
+	for(i=0; i<STR_LEN*3*8; i++) {
 #ifdef ONE
 		__R30 |= 0x1<<out;		// Set the GPIO pin to 1
 		__delay_cycles(oneCyclesOn-1);
@@ -32,4 +35,9 @@ void main(void)
 		__delay_cycles(zeroCyclesOff-2);
 #endif
 	}
+	// Send Reset
+	__R30 &= ~(0x1<<out);	// Clear the GPIO pin
+	__delay_cycles(resetCycles);
+	
+	__halt();
 }
--- docs/05blocks/code/neo2.c
+++ docs/05blocks/code/neo2.c
@@ -3,14 +3,13 @@
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
 
+#define STR_LEN 3
 #define	oneCyclesOn		700/5	// Stay on 700ns
 #define oneCyclesOff	800/5
 #define zeroCyclesOn	350/5
 #define zeroCyclesOff	600/5
+#define resetCycles		60000/5	// Must be at least 50u, use 60u
 #define out 1		// Bit number to output one
-#define STRING_LEN		24
-
-// #define ONE
 
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
@@ -20,11 +19,11 @@ void main(void)
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 	
-	uint32_t color[STRING_LEN] = {0x0f000000, 0x000f0000, 0x0000f00};	// green, red, blue
-	uint32_t i, j;
+	uint32_t color[STR_LEN] = {0x0f0000, 0x000f00, 0x0000f};	// green, red, blue
+	int i, j;
 
-	for(j=0; j<3; j++) {
-		for(i=31; i>=8; i--) {
+	for(j=0; j<STR_LEN; j++) {
+		for(i=23; i>=0; i--) {
 			if(color[j] & (0x1<<i)) {
 				__R30 |= 0x1<<out;		// Set the GPIO pin to 1
 				__delay_cycles(oneCyclesOn-1);
@@ -38,4 +37,9 @@ void main(void)
 			}
 		}
 	}
+	// Send Reset
+	__R30 &= ~(0x1<<out);	// Clear the GPIO pin
+	__delay_cycles(resetCycles);
+	
+	__halt();
 }
