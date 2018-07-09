92d64417c4b07332ffa9ca32938f77535472b646
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-07-09 19:25:56 -0400

    Example without while()

--- docs/08more/code/cycle.c
+++ docs/08more/code/cycle.c
@@ -9,20 +9,23 @@
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
-volatile uint32_t cycle, stall;		// volatile forces it to DRAM
-
 void main(void)
 {
-	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
+	// These will be kept in registers and never witten to DRAM
+	uint32_t cycle, stall;
+
+	// Clear SYSCFG[STANDBY_INIT] to enable OCP master port
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 	
 	PRU0_CTRL.CTRL_bit.CTR_EN = 1;	// Enable cycle counter
 
-	while(1) {
-		__R30 |= 0x1<<out;			// Set the GPIO pin to 1
-		PRU0_CTRL.CYCLE = 0;		// Reset cycle counter
-		__R30 &= ~(0x1<<out);		// Clear the GPIO pin
-		cycle = PRU0_CTRL.CYCLE;	// Read cycles and store in DRAM
-		stall = PRU0_CTRL.STALL;
-	}
+	__R30 |= 0x1<<out;			// Set the GPIO pin to 1
+	// Reset cycle counter, cycle is on the right side to force the compiler
+	// to put it in it's own register
+	PRU0_CTRL.CYCLE = cycle;
+	__R30 &= ~(0x1<<out);		// Clear the GPIO pin
+	cycle = PRU0_CTRL.CYCLE;	// Read cycle and store in a register
+	stall = PRU0_CTRL.STALL;	// Ditto for stall
+
+	__halt();
 }
