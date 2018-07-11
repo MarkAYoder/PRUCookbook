54119d247a33078b951ee99368613aaa0d7b645f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-07-11 16:23:58 -0400

    Removed j

--- docs/01start/code/hello.c
+++ docs/01start/code/hello.c
@@ -1,13 +1,6 @@
 #include <stdint.h>
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
-// #include <pru_ctrl.h>
-// #include <stddef.h>
-// #include <rsc_types.h>
-
-#define	INS_PER_US 200           // 5ns per instruction
-#define INS_PER_DELAY_LOOP 2	 // two instructions per delay loop
-#define DELAY_CYCLES_US (INS_PER_US / INS_PER_DELAY_LOOP)
 
 #define GPIO1 0x4804C000
 #define GPIO_CLEARDATAOUT 0x190
@@ -23,14 +16,14 @@ volatile register unsigned int __R30;
 volatile register unsigned int __R31;
 
 void main(void) {
-	int i, j;
+	int i;
 
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
 	for(i=0; i<5; i++) {
 		*GPIO1_SET = USR3;      // The the USR3 LED on
-		__delay_cycles(500000000/5); 
+		__delay_cycles(500000000/5);    // Wait 1/2 second
 
 		*GPIO1_CLEAR = USR3;
 		 __delay_cycles(500000000/5); 
