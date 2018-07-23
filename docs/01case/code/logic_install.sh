ab0b2d4a559ff62850a168eb846c86733d9032fa
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-07-23 12:17:17 -0400

    Adding beaglelogic install instructions

---
+++ docs/01case/code/logic_install.sh
@@ -0,0 +1,14 @@
+# Instructions for installing BeagleLogic
+# From: https://github.com/abhishek-kakkar/BeagleLogic/wiki
+wget https://github.com/abhishek-kakkar/BeagleLogic/archive/release-v1.1.tar.gz
+tar xaf release-v1.1.tar.gz
+
+# First PRU
+cd BeagleLogic-release-v1.1/cd BeagleLogic-release-v1.1/
+make
+
+sudo cp beaglelogic-pru0 /lib/firmware/am335x-pru0-fw
+sudo cp beaglelogic-pru1 /lib/firmware/am335x-pru1-fw
+
+# Now the kernel driver
+sudo apt install linux-headers-`uname -r`
