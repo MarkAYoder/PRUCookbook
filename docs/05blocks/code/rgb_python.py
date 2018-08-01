612f766b8c048441c1f2075e8ce4d95e2adae916
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-08-01 15:04:34 -0400

    RGB working

--- docs/05blocks/code/rgb_python.py
+++ docs/05blocks/code/rgb_python.py
@@ -8,6 +8,11 @@ LAT="P1_36"
 CLK="P1_33"
 
 R1="P2_10"
+R2="P2_4"
+B1="P2_6"
+B2="P2_1"
+G1="P2_8"
+G2="P2_2"
 
 LA="P2_32"
 LB="P2_30"
@@ -19,6 +24,11 @@ GPIO.setup(LAT, GPIO.OUT)
 GPIO.setup(CLK, GPIO.OUT)
 
 GPIO.setup(R1, GPIO.OUT)
+GPIO.setup(R2, GPIO.OUT)
+GPIO.setup(B1, GPIO.OUT)
+GPIO.setup(B2, GPIO.OUT)
+GPIO.setup(G1, GPIO.OUT)
+GPIO.setup(G2, GPIO.OUT)
 
 GPIO.setup(LA, GPIO.OUT)
 GPIO.setup(LB, GPIO.OUT)
@@ -30,14 +40,27 @@ GPIO.output(LAT, 0)
 
 while True:
     for bank in range(16):
-        GPIO.output(LA, bank&0x1)
+        GPIO.output(LA, bank>>0&0x1)
+        GPIO.output(LB, bank>>1&0x1)
+        GPIO.output(LC, bank>>2&0x1)
+        GPIO.output(LD, bank>>3&0x1)
         for i in range(64):
             GPIO.output(R1,  1)
+            GPIO.output(B1,  1)
+            GPIO.output(G1,  1)
+            GPIO.output(R2,  1)
+            GPIO.output(B2,  0)
+            GPIO.output(G2,  0)
     
             GPIO.output(CLK, 0)
             GPIO.output(CLK, 1)
     
             GPIO.output(R1,  0)
+            GPIO.output(B1,  0)
+            GPIO.output(G1,  0)
+            GPIO.output(R2,  0)
+            GPIO.output(B2,  0)
+            GPIO.output(G2,  1)
     
             GPIO.output(CLK, 0)
             GPIO.output(CLK, 1)
