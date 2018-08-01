fac9ed0b0d234bde2e61d8fff0688cf2c4ede2a8
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-08-01 15:09:09 -0400

    Added comments

--- docs/05blocks/code/rgb_python.py
+++ docs/05blocks/code/rgb_python.py
@@ -40,32 +40,33 @@ GPIO.output(LAT, 0)
 
 while True:
     for bank in range(16):
-        GPIO.output(LA, bank>>0&0x1)
+        GPIO.output(LA, bank>>0&0x1)    # Select rows
         GPIO.output(LB, bank>>1&0x1)
         GPIO.output(LC, bank>>2&0x1)
         GPIO.output(LD, bank>>3&0x1)
-        for i in range(64):
-            GPIO.output(R1,  1)
+        for i in range(16):
+            GPIO.output(R1,  1)     # Top row
             GPIO.output(B1,  1)
             GPIO.output(G1,  1)
-            GPIO.output(R2,  1)
+            
+            GPIO.output(R2,  1)     # Bottom row
             GPIO.output(B2,  0)
             GPIO.output(G2,  0)
-    
-            GPIO.output(CLK, 0)
+
+            GPIO.output(CLK, 0)     # Toggle clock
             GPIO.output(CLK, 1)
     
-            GPIO.output(R1,  0)
+            GPIO.output(R1,  0)     # Top row
             GPIO.output(B1,  0)
             GPIO.output(G1,  0)
-            GPIO.output(R2,  0)
+            GPIO.output(R2,  0)     # Bottom row
             GPIO.output(B2,  0)
             GPIO.output(G2,  1)
     
-            GPIO.output(CLK, 0)
+            GPIO.output(CLK, 0)     # Toggle clock
             GPIO.output(CLK, 1)
     
-        GPIO.output(OE,  1)
-        GPIO.output(LAT, 1)
+        GPIO.output(OE,  1)     # Disable display
+        GPIO.output(LAT, 1)     # Toggle latch
         GPIO.output(LAT, 0)
-        GPIO.output(OE,  0)
+        GPIO.output(OE,  0)     # Enable display
