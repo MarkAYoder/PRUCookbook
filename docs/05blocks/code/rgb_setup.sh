48bb9510d449b29cd570e689bb11df46030c0e0a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-08-01 09:58:46 -0400

    chmod +x on setup files

--- docs/05blocks/code/rgb_setup.sh
+++ docs/05blocks/code/rgb_setup.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-#
-export PRUN=0
-export TARGET=input1
-echo PRUN=$PRUN
-echo TARGET=$TARGET
+# Setup for 64x32 RGB Matrix
+# export PRUN=0
+# export TARGET=pwm7
+# echo PRUN=$PRUN
+# echo TARGET=$TARGET
 
 # Configure the PRU pins based on which Beagle is running
 machine=$(awk '{print $NF}' /proc/device-tree/model)
 echo -n $machine
 if [ $machine = "Black" ]; then
     echo " Found"
-    config-pin P9_31 pruout
-    config-pin -q P9_31
-    config-pin P9_25 pruin
-    config-pin -q P9_25
+    pins=""
 elif [ $machine = "Blue" ]; then
     echo " Found"
     pins=""
 elif [ $machine = "PocketBeagle" ]; then
     echo " Found"
-    config-pin P1_36 pruout
-    config-pin -q P1_36
-    config-pin P1_29 pruin
-    config-pin -q P1_29
+    prupins="P2_32 P1_31 P1_33 P1_29"
+    gpioping=
 else
     echo " Not Found"
     pins=""
 fi
+
+for pin in $pins
+do
+    echo $pin
+    # config-pin $pin pruout
+    config-pin -q $pin
+done
