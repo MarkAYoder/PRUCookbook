3c73747886ba1a3e62afdcadde76d7b3be615d23
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2018-08-02 11:40:57 -0400

    Adding comments

--- docs/05blocks/code/rgb1.c
+++ docs/05blocks/code/rgb1.c
@@ -3,7 +3,10 @@
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
 
-// These are from 
+// These are from https://github.com/FalconChristmas/fpp/blob/master/src/pru/PocketScrollerV1.hp
+// _gpio tells which gpio port and _pin tells which bit in the port
+// The first 1 in r11 is for the J1 connector
+// See the githuub file for the other connectors
 #define r11_gpio 1
 #define r11_pin 20
 #define g11_gpio 1
@@ -23,32 +26,30 @@
 #define gpio_oe    21
 #define gpio_clock 15
 
-#define gpio_sel0 16 /* must be sequential with sel1 and sel2 */
+#define gpio_sel0 16	// must be sequential with sel1 and sel2
 #define gpio_sel1 17
 #define gpio_sel2 18
 #define gpio_sel3 19
 #define gpio_sel4 20
 
-
-#define pru_latch  0
+#define pru_latch  0	// These are the bit positions on R30
 #define pru_oe     7
 #define pru_clock  1
 
-#define pru_sel0   2
+#define pru_sel0   2	// These are called LA, LB, LC and LD in the python code
 #define pru_sel1   3
 #define pru_sel2   4
 #define pru_sel3   5
 #define pru_sel4   6
 
 // This is mine
-#define GPIO0	0x44e07000		// GPIO Bank 0  See Table 2.2 of TRM <1>
+#define GPIO0	0x44e07000		// GPIO Bank 0  See Table 2.2 of TRM
 #define GPIO1	0x4804c000		// GPIO Bank 1
 #define GPIO2	0x481ac000		// GPIO Bank 2
 #define GPIO3	0x481ae000		// GPIO Bank 3
 #define GPIO_CLEARDATAOUT	0x190	// For clearing the GPIO registers
 #define GPIO_SETDATAOUT		0x194	// For setting the GPIO registers
 #define GPIO_DATAOUT		0x138	// For reading the GPIO registers
-#define P9_11	(0x1<<30)			// Bit position tied to P9_11
 
 #define DELAY 100
 
@@ -57,6 +58,7 @@ volatile register uint32_t __R31;
 
 void main(void)
 {
+	// Set up the pointers to each of the GPIO ports 
 	uint32_t *gpio0 = (uint32_t *)GPIO0;
 	uint32_t *gpio1 = (uint32_t *)GPIO1;
 	uint32_t *gpio2 = (uint32_t *)GPIO2;
@@ -76,16 +78,16 @@ void main(void)
 	        __delay_cycles(DELAY);
 	        
     	    for(i=0; i<64; i++) {
-    	      	gpio1[GPIO_SETDATAOUT/4]   = (0x1 << r11_pin);
+    	      	gpio1[GPIO_SETDATAOUT/4] = (0x1 << r11_pin);
     	    	__delay_cycles(DELAY);
-    	      	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << g11_pin);
+    	      	gpio1[GPIO_SETDATAOUT/4] = (0x1 << g11_pin);
     	    	__delay_cycles(DELAY);
-    	      	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << b11_pin);
+    	      	gpio1[GPIO_SETDATAOUT/4] = (0x1 << b11_pin);
     	    	__delay_cycles(DELAY);
     	      	
-    	      	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << r12_pin);
+    	      	gpio1[GPIO_SETDATAOUT/4]   = (0x1 << r12_pin);
     	    	__delay_cycles(DELAY);
-    	      	gpio1[GPIO_SETDATAOUT/4]   = (0x1 << g12_pin);
+    	      	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << g12_pin);
     	    	__delay_cycles(DELAY);
     	      	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << b12_pin);
     	    	__delay_cycles(DELAY);
@@ -99,14 +101,14 @@ void main(void)
     	    	__delay_cycles(DELAY);
     	      	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << g11_pin);
     	    	__delay_cycles(DELAY);
-    	      	gpio1[GPIO_SETDATAOUT/4]   = (0x1 << b11_pin);
+    	      	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << b11_pin);
     	    	__delay_cycles(DELAY);
     	      	
     	    	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << r12_pin);
     	    	__delay_cycles(DELAY);
-    	      	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << g12_pin);
+    	      	gpio1[GPIO_SETDATAOUT/4]   = (0x1 << g12_pin);
     	    	__delay_cycles(DELAY);
-    	      	gpio1[GPIO_SETDATAOUT/4]   = (0x1 << b12_pin);
+    	      	gpio1[GPIO_CLEARDATAOUT/4] = (0x1 << b12_pin);
     	    	__delay_cycles(DELAY);
     	      	
                 __R30 |=  (0x1<<pru_clock);      // Toggle clock
