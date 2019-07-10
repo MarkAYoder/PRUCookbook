999ef7c96ebb193d824e273d6138d8c3e0ad6497
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-07-10 13:33:55 -0400

    Switching to AI

--- docs/02start/code/setup.sh
+++ docs/02start/code/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-export PRUN=0
+export PRUN=1_0
 export TARGET=hello
+export MODEL=AI
+export CHIP=am57xx
 echo PRUN=$PRUN
 echo TARGET=$TARGET
+echo MODEL=$MODEL
+echo CHIP=$CHIP
 
 echo none > /sys/class/leds/beaglebone\:green\:usr3/trigger
