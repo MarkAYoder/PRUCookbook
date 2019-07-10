c330d74df5b38ce4408265ca65f10dd3e1cbb57f
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-07-10 14:25:54 -0400

    Added all GPIO addresses

--- docs/02start/code/hello.c
+++ docs/02start/code/hello.c
@@ -3,14 +3,26 @@
 #include "resource_table_empty.h"
 
 #define GPIO1 0x4804C000
+#define GPIO2 0x48055000
+#define GPIO3 0x48057000
+#define GPIO4 0x48059000
+#define GPIO5 0x4805B000
+#define GPIO6 0x4805D000
+#define GPIO7 0x48051000
+#define GPIO8 0x48053000
 #define GPIO_CLEARDATAOUT 0x190
 #define GPIO_SETDATAOUT 0x194
-#define USR0 (1<<21)
-#define USR1 (1<<22)
-#define USR2 (1<<23)
-#define USR3 (1<<24)
-unsigned int volatile * const GPIO1_CLEAR = (unsigned int *) (GPIO1 + GPIO_CLEARDATAOUT);
-unsigned int volatile * const GPIO1_SET   = (unsigned int *) (GPIO1 + GPIO_SETDATAOUT);
+// GPIO3
+#define USR0 (1<<17)
+#define USR2 (1<<15)
+#define USR3 (1<<14)
+#define USR4 (1<< 7)
+// GPIO5
+#define USR1 (1<< 5)
+
+
+unsigned int volatile * const GPIO1_CLEAR = (unsigned int *) (GPIO3 + GPIO_CLEARDATAOUT);
+unsigned int volatile * const GPIO1_SET   = (unsigned int *) (GPIO3 + GPIO_SETDATAOUT);
 
 volatile register unsigned int __R30;
 volatile register unsigned int __R31;
