4df400f16e0bf1b953a03cd4f80d54c66552b9d3
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-07-19 08:25:34 -0400

    Added P8_17 and P9_25

--- docs/02start/code/hello2AI.c
+++ docs/02start/code/hello2AI.c
@@ -16,9 +16,9 @@ void main(void) {
 	uint32_t *gpio3 = (uint32_t *)GPIO3;
 	// uint32_t *gpio4 = (uint32_t *)GPIO4;
 	uint32_t *gpio5 = (uint32_t *)GPIO5;
-	// uint32_t *gpio6 = (uint32_t *)GPIO6;
+	uint32_t *gpio6 = (uint32_t *)GPIO6;
 	// uint32_t *gpio7 = (uint32_t *)GPIO7;
-	// uint32_t *gpio8 = (uint32_t *)GPIO8;
+	uint32_t *gpio8 = (uint32_t *)GPIO8;
 	
 	// Select which pins to toggle.  These are all on pru1_1
 	uint32_t gpio = P9_14 | P9_16 | P8_15 | P8_16 | P8_26;
@@ -26,13 +26,11 @@ void main(void) {
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	for(i=0; i<10; i++) {
-		gpio5[GPIO_SETDATAOUT]   = USR1;			// The the USR3 LED on
+	for(i=0; i<100; i++) {
+		gpio5[GPIO_SETDATAOUT]   = USR1;		// Turn the USR1 LED on
 		gpio3[GPIO_CLEARDATAOUT] = USR2;
-		// gpio4[GPIO_SETDATAOUT]   = (1<<29);
-		// gpio8[GPIO_SETDATAOUT]   = (1<<18);
-		// gpio4[GPIO_SETDATAOUT]   = (1<<28);
-		// gpio6[GPIO_SETDATAOUT]   = (1<<17);
+		gpio8[GPIO_SETDATAOUT]   = P8_17;
+		gpio6[GPIO_SETDATAOUT]   = P9_25;
 		
 		__R30 |= gpio;		// Set the GPIO pin to 1
 
@@ -40,10 +38,8 @@ void main(void) {
 
 		gpio5[GPIO_CLEARDATAOUT] = USR1;
         gpio3[GPIO_SETDATAOUT]   = USR2;
-		// gpio4[GPIO_CLEARDATAOUT] = (1<<29);
-		// gpio8[GPIO_CLEARDATAOUT] = (1<<18);
-		// gpio4[GPIO_CLEARDATAOUT] = (1<<28);
-		// gpio6[GPIO_CLEARDATAOUT] = (1<<17);
+		gpio8[GPIO_CLEARDATAOUT] = P8_17;
+		gpio6[GPIO_CLEARDATAOUT] = P9_25;
 		
 		__R30 &= ~gpio;		// Clearn the GPIO pin
 
@@ -63,6 +59,7 @@ void main(void) {
 const char init_pins[] =  
 	"/sys/class/leds/beaglebone:green:usr1/trigger\0none\0" \
 	"/sys/class/leds/beaglebone:green:usr2/trigger\0none\0" \
-	"/sys/class/leds/beaglebone:green:usr3/trigger\0none\0" \
+	"/sys/class/gpio/export\0 177\0" \
+	"/sys/class/gpio/gpio177/direction\0out\0" \
 	"\0\0";
 
