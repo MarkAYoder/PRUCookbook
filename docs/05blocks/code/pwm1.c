e30750fe526d6079db8f714d8a686e0a5fe91b13
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2019-07-19 08:37:28 -0400

    Added P9_14

--- docs/05blocks/code/pwm1.c
+++ docs/05blocks/code/pwm1.c
@@ -1,18 +1,19 @@
 #include <stdint.h>
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
+#include "prugpio.h"
+
+#define PRUN 1_1
 
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
 void main(void)
 {
-	uint32_t gpio;
+	uint32_t gpio = P9_14;	// Select which pin to toggle.;
 
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
-
-	gpio = 0x0001;	// Select which pin to toggle.
 
 	while (1) {
 		__R30 |= gpio;		// Set the GPIO pin to 1
