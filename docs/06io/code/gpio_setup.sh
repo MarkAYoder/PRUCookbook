ca25f6e402d7679dff55eda1dc04ad6f554e4a6c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-06-03 10:50:03 -0400

    Updated Commented out config-pin

--- docs/06io/code/gpio_setup.sh
+++ docs/06io/code/gpio_setup.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-export PRUN=0
-export TARGET=gpio1
-echo PRUN=$PRUN
+
+export TARGET=gpio1.pru0
 echo TARGET=$TARGET
 
-# Configure the PRU pins based on which Beagle is running
-machine=$(awk '{print $NF}' /proc/device-tree/model)
-echo -n $machine
-if [ $machine = "Black" ]; then
-    echo " Found"
-    pins="P9_11"
-elif [ $machine = "Blue" ]; then
-    echo " Found"
-    pins=""
-elif [ $machine = "PocketBeagle" ]; then
-    echo " Found"
-    pins="P1_36"
-else
-    echo " Not Found"
-    pins=""
-fi
+# # Configure the PRU pins based on which Beagle is running
+# machine=$(awk '{print $NF}' /proc/device-tree/model)
+# echo -n $machine
+# if [ $machine = "Black" ]; then
+#     echo " Found"
+#     pins="P9_11"
+# elif [ $machine = "Blue" ]; then
+#     echo " Found"
+#     pins=""
+# elif [ $machine = "PocketBeagle" ]; then
+#     echo " Found"
+#     pins="P1_36"
+# else
+#     echo " Not Found"
+#     pins=""
+# fi
 
-for pin in $pins
-do
-    echo $pin
-    config-pin $pin out
-    config-pin -q $pin
-done
+# for pin in $pins
+# do
+#     echo $pin
+#     config-pin $pin gpio
+#     config-pin -q $pin
+# done
