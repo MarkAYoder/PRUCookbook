d939f54aeb0ffce235d43f53c55dcb778fdb010c
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-06-03 10:54:43 -0400

    Made executable

--- docs/03details/code/encoder_setup.sh
+++ docs/03details/code/encoder_setup.sh
@@ -1,1 +1,49 @@
-../../01case/code/encoder_setup.sh
+#!/bin/bash
+# Configure the pins based on which Beagle is running
+machine=$(awk '{print $NF}' /proc/device-tree/model)
+echo -n $machine
+
+# Configure eQEP pins
+if [ $machine = "Black" ]; then
+    echo " Found"
+    pins="P9_92 P9_27 P8_35 P8_33 P8_12 P8_11 P8_41 P8_42"
+elif [ $machine = "Blue" ]; then
+    echo " Found"
+    pins=""
+elif [ $machine = "PocketBeagle" ]; then
+    echo " Found"
+    pins="P1_31 P2_34 P2_10 P2_24 P2_33"
+else
+    echo " Not Found"
+    pins=""
+fi
+
+for pin in $pins
+do
+    echo $pin
+    config-pin $pin qep
+    config-pin -q $pin
+done
+
+##########################################
+# Configure PRU pins
+if [ $machine = "Black" ]; then
+    echo " Found"
+    pins="P8_16 P8_15"
+elif [ $machine = "Blue" ]; then
+    echo " Found"
+    pins=""
+elif [ $machine = "PocketBeagle" ]; then
+    echo " Found"
+    pins="P2_09 P2_18"
+else
+    echo " Not Found"
+    pins=""
+fi
+
+for pin in $pins
+do
+    echo $pin
+    config-pin $pin pruin
+    config-pin -q $pin
+done
--- docs/03details/code/servos_setup.sh
+++ docs/03details/code/servos_setup.sh
@@ -1,1 +1,24 @@
-../../01case/code/servos_setup.sh
+#!/bin/bash
+# Configure the PRU pins based on which Beagle is running
+machine=$(awk '{print $NF}' /proc/device-tree/model)
+echo -n $machine
+if [ $machine = "Black" ]; then
+    echo " Found"
+    pins="P8_27 P8_28 P8_29 P8_30 P8_39 P8_40 P8_41 P8_42"
+elif [ $machine = "Blue" ]; then
+    echo " Found"
+    pins=""
+elif [ $machine = "PocketBeagle" ]; then
+    echo " Found"
+    pins="P2_35 P1_35 P1_02 P1_04"
+else
+    echo " Not Found"
+    pins=""
+fi
+
+for pin in $pins
+do
+    echo $pin
+    config-pin $pin pruout
+    config-pin -q $pin
+done
