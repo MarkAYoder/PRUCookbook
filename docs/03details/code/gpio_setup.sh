65771e03e4134c69bda30792626daa0dd368ed16
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-06-03 15:25:35 -0400

    Switched back to config-pin ... out

--- docs/03details/code/gpio_setup.sh
+++ docs/03details/code/gpio_setup.sh
@@ -3,26 +3,26 @@
 export TARGET=gpio.pru0
 echo TARGET=$TARGET
 
-# # Configure the PRU pins based on which Beagle is running
-# machine=$(awk '{print $NF}' /proc/device-tree/model)
-# echo -n $machine
-# if [ $machine = "Black" ]; then
-#     echo " Found"
-#     pins="P9_11"
-# elif [ $machine = "Blue" ]; then
-#     echo " Found"
-#     pins=""
-# elif [ $machine = "PocketBeagle" ]; then
-#     echo " Found"
-#     pins="P1_36"
-# else
-#     echo " Not Found"
-#     pins=""
-# fi
+# Configure the PRU pins based on which Beagle is running
+machine=$(awk '{print $NF}' /proc/device-tree/model)
+echo -n $machine
+if [ $machine = "Black" ]; then
+    echo " Found"
+    pins="P9_11"
+elif [ $machine = "Blue" ]; then
+    echo " Found"
+    pins=""
+elif [ $machine = "PocketBeagle" ]; then
+    echo " Found"
+    pins="P1_36"
+else
+    echo " Not Found"
+    pins=""
+fi
 
-# for pin in $pins
-# do
-#     echo $pin
-#     config-pin $pin gpio
-#     config-pin -q $pin
-# done
+for pin in $pins
+do
+    echo $pin
+    config-pin $pin out
+    config-pin -q $pin
+done
