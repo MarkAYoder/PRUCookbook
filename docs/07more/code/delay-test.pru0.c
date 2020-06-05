71d6ce68f5ad4ca47d8159f279c7560d806d33ff
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-06-05 13:48:37 -0400

    Updated asm examples

--- docs/07more/code/delay-test.pru0.c
+++ docs/07more/code/delay-test.pru0.c
@@ -2,26 +2,27 @@
 #include <stdint.h>
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
+#include "prugpio.h"
 
 // The function is defined in delay.asm in same dir
 // We just need to add a declaration here, the defination can be
 // seperately linked
 extern void my_delay_cycles(uint32_t);
 
-#define out 1		// Bit number to output on
-
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
 void main(void)
 {
+	uint32_t gpio = P9_31;	// Select which pin to toggle.;
+
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
 	while(1) {
-		__R30 |= 0x1<<out;		// Set the GPIO pin to 1
+		__R30 |= gpio;		// Set the GPIO pin to 1
 		my_delay_cycles(1);
-		__R30 &= ~(0x1<<out);	// Clear the GPIO pin
+		__R30 &= ~gpio;		// Clear the GPIO pin
 		my_delay_cycles(1);
 	}
 }
