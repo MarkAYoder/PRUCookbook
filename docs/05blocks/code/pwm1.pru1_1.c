50e9b490acc104fcec51c9fdb06dae82b5bb51ed
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2020-06-10 16:04:29 -0400

    Converted to AI

---
+++ docs/05blocks/code/pwm1.pru1_1.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <pru_cfg.h>
+#include "resource_table_empty.h"
+#include "prugpio.h"
+
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
+
+void main(void)
+{
+	uint32_t gpio = P9_16;	// Select which pin to toggle.;
+
+	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
+	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+	while(1) {
+		__R30 |= gpio;		// Set the GPIO pin to 1
+		__delay_cycles(100000000);
+		__R30 &= ~gpio;		// Clear the GPIO pin
+		__delay_cycles(100000000);
+	}
+}
