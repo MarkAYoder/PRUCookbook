3b7100980c7413d6dfe4e2a79000ed6299872078
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2021-07-29 13:18:53 -0400

    Switched to buffered output

--- docs/05blocks/code/neo-rainbow.py
+++ docs/05blocks/code/neo-rainbow.py
@@ -9,17 +9,17 @@ shift = 3
 phase = 0
 
 # Open a file
-fo = open("/dev/rpmsg_pru30", "w", 0)
+fo = open("/dev/rpmsg_pru30", "wb", 0)
 
 while True:
     for i in range(0, len):
         r = (amp * (math.sin(2*math.pi*f*(i-phase-0*shift)/len) + 1)) + 1;
         g = (amp * (math.sin(2*math.pi*f*(i-phase-1*shift)/len) + 1)) + 1;
         b = (amp * (math.sin(2*math.pi*f*(i-phase-2*shift)/len) + 1)) + 1;
-        fo.write("%d %d %d %d\n" % (i, r, g, b))
+        fo.write(b"%d %d %d %d\n" % (i, r, g, b))
         # print("0 0 127 %d" % (i))
 
-    fo.write("-1 0 0 0\n");
+    fo.write(b"-1 0 0 0\n");
     phase = phase + 1
     sleep(0.05)
 
